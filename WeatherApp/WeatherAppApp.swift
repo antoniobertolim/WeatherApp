f911b02d50cadcb6a5db874252b1e762f1d0fae9
Antonio Bertolim <abertolim@gmail.com>
2021-10-19 00:43:37 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Antonio Bertolim on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
